51049510c65a65a02330ad938fdba0aea2658a22
datmax <max.datsun@student.univaq.it>
2021-03-22 12:13:41 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-left">
+        <h1>Link utili</h1>
+        <v-card class="my-6">
+          <v-card-title class="yellow--text">
+            Nome casa scommesse
+          </v-card-title>
+          <v-card-text>
+            <div>
+              <h1>
+                Bonus:
+              </h1>
+              <h2>
+                Ciao
+              </h2>
+              <h2>
+                Ciao
+              </h2>
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="yellow" class="black--text">
+              Ottieni bonus
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-card class="my-6">
+          <v-card-title class="yellow--text">
+            Nome casa scommesse
+          </v-card-title>
+          <v-card-text>
+            <div>
+              <h1>
+                Bonus:
+              </h1>
+              <h2>
+                Ciao
+              </h2>
+              <h2>
+                Ciao
+              </h2>
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="yellow" class="black--text">
+              Ottieni bonus
+            </v-btn>
+          </v-card-actions>
+        </v-card><v-card class="my-6">
+          <v-card-title class="yellow--text">
+            Nome casa scommesse
+          </v-card-title>
+          <v-card-text>
+            <div>
+              <h1>
+                Bonus:
+              </h1>
+              <h2>
+                Ciao
+              </h2>
+              <h2>
+                Ciao
+              </h2>
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="yellow" class="black--text">
+              Ottieni bonus
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  head () {
+    return {
+      title: '13Tipster'
+    }
+  }
+}
+</script>
+<style>
+h2, h1{
+  margin-bottom: 2px;
+}
+</style>
